1a82b2ee1521962873069725f0d8bcec8e3952b4
U-hyeon <wisebuddyu@gmail.com>
2025-03-21 01:48:13 +0900

    오류수정

--- src/main/java/com/aqours_challenge/our_challenge/dto/MemberFormDto.java
+++ src/main/java/com/aqours_challenge/our_challenge/dto/MemberFormDto.java
@@ -14,8 +14,6 @@ import org.hibernate.validator.constraints.Length;
  *     <li>주소</li>
  * </sl>
  */
-@Getter
-@Setter
 public class MemberFormDto {
     /**
      * 회원명
@@ -42,4 +40,36 @@ public class MemberFormDto {
      */
     @NotBlank(message = "트위터 아이디는 필수 입력 값입니다.")
     private String twitterId;
+
+    public @NotBlank(message = "닉네임은 필수 입력 값입니다.") String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(@NotBlank(message = "닉네임은 필수 입력 값입니다.") String memberName) {
+        this.memberName = memberName;
+    }
+
+    public @NotBlank(message = "이메일은 필수 입력 값입니다.") @Email(message = "이메일 형식으로 입력해주세요.") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "이메일은 필수 입력 값입니다.") @Email(message = "이메일 형식으로 입력해주세요.") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "비밀번호는 필수 입력 값입니다.") @Length(min = 8, max = 16, message = "비밀번호는 8~16자 이어야 합니다.") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "비밀번호는 필수 입력 값입니다.") @Length(min = 8, max = 16, message = "비밀번호는 8~16자 이어야 합니다.") String password) {
+        this.password = password;
+    }
+
+    public @NotBlank(message = "트위터 아이디는 필수 입력 값입니다.") String getTwitterId() {
+        return twitterId;
+    }
+
+    public void setTwitterId(@NotBlank(message = "트위터 아이디는 필수 입력 값입니다.") String twitterId) {
+        this.twitterId = twitterId;
+    }
 }
--- src/main/java/com/aqours_challenge/our_challenge/entity/Member.java
+++ src/main/java/com/aqours_challenge/our_challenge/entity/Member.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
-@Getter
-@Setter
 public class Member {
     @Id
     @Column(name = "member_id")
@@ -26,6 +24,70 @@ public class Member {
     private String gender;
     private String age;
     private String twitterId;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTwitterId() {
+        return twitterId;
+    }
+
+    public void setTwitterId(String twitterId) {
+        this.twitterId = twitterId;
+    }
 
     public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
         Member member = new Member();
