2167151f79b6d4e872564faa34b1a09742744373
U-hyeon <110391192+U-hyeon@users.noreply.github.com>
2025-03-29 22:46:16 +0900

    태그, 태그-포스트 엔티티 작성

---
+++ src/main/java/com/aqours_challenge/our_challenge/entity/Tag.java
@@ -0,0 +1,31 @@
+package com.aqours_challenge.our_challenge.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Tag")
+public class Tag {
+    @Id
+    @Column(name = "tag_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long tagId;
+
+    @Column(name="tag_name", unique=true)
+    private String tagName;
+
+    public Long getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(Long tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+}
